de2d89aa7160b47906699cb6acb5a0ff926e94a1
dev-devi <radixdt.1499@gmail.com>
2022-08-24 12:40:11 +0530

    🚀 [Add] architecture setup module

---
+++ FitnessVision/FitnessVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessVisionApp.swift
+//  FitnessVision
+//
+//  Created by Devangi Prajapati on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessVisionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainView()
+    }
+  }
+}
